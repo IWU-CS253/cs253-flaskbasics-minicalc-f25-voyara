5fafc7c8d1faa446416c8be7c36965d88f931b9b
Hirat Rahi <hrahi@iwu.edu>
2025-09-26 15:56:40 -0500

    added everything about wordstats

--- calc.py
+++ calc.py
@@ -1,4 +1,6 @@
 # calc.py
+import math
+
 
 def calculate(current_value, num, clear):
     if clear:
@@ -15,3 +17,38 @@ def calculate(current_value, num, clear):
             # Append the pressed button value to the current value
             return current_value + num
     return current_value
+
+
+"""
+# calc.py
+import math
+
+
+def calculate(current_value, num, clear):
+    if clear:
+        return ''  # Clear the current value if clear button is pressed
+    elif num:
+        if num == '=':
+            # Calculate result if '=' is pressed
+            try:
+                # Safely evaluate the current expression
+                return str(eval(current_value))
+            except:
+                return 'Error'
+        elif num == '√':
+            # calculate the result if "√" is pressed
+            try:
+                return str(math.sqrt(float(current_value)))  # return the square root of it
+            except:
+                return 'Error'
+        elif num == '!':
+            # calculate the factorial if "!" is pressed
+            try:
+                return str(math.factorial(int(current_value)))   # return the factorial of the number
+            except:
+                return 'Error'
+        else:
+            # Append the pressed button value to the current value
+            return current_value + num
+    return current_value
+"""
